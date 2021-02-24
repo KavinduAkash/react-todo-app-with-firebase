b781c294250b5697b43bf826d1bdfbd938a39322
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-02-24 22:18:27 +0530

    style card component and add input section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import {Col, Row} from "reactstrap";
+import {Col, Input, Row} from "reactstrap";
 import './App.style.scss';
 import Card from "./components/card";
+import {Button} from "antd";
 
 class App extends React.Component{
   render() {
-    return (
+
+      let date = new Date().toDateString();
+
+      return (
       <div className={'todo-app'}>
           <div className={'header'}>
             <h2>Todo App</h2>
+              <p>{date}</p>
           </div>
           <div className={'content'}>
+
+              <div className={'app-input'}>
+                  <Input type={'text'} placeholder={'Title'}/>
+                  <Input type={'textarea'} placeholder={'Description'}/>
+                  <div className={'text-right'}>
+                      <Button>Add to todo list</Button>
+                  </div>
+              </div>
+
+              <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
             <Card/>
           </div>
       </div>
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './card.style.scss';
 import {Col, Input, Row} from "reactstrap";
+import { Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons'
 import './card.style.scss';
 
 class Card extends React.Component {
@@ -10,10 +12,12 @@ class Card extends React.Component {
 
                 <Row>
                     <Col sm={10} md={10} lg={10} xl={10}>
-                        <h3>Title</h3>
+                        <div className={'card-heading'}>
+                            <h3>Title</h3>
+                        </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad animi assumenda aut</p>
                     </Col>
-                    <Col sm={2} md={2} lg={2} xl={2}>
+                    <Col className={'card-checkbox'} sm={2} md={2} lg={2} xl={2}>
                         <Input type="checkbox" />
                     </Col>
                 </Row>
